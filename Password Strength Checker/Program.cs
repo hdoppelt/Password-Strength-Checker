a1df1e338f710a1f28f8b7120c2db6c18367786f
Harrison Doppelt <hdoppelt@gmail.com>
2025-02-15 15:20:07 -0700

    Add project files.

---
+++ Password Strength Checker/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Password_Strength_Checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
